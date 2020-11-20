dbcbb078067bd6078189e379377af940cb3b23f1
gmoraitis <moraitis.georgios@icloud.com>
2020-11-20 18:47:38 +0200

    Changes for sliders and buttons

--- js/eq.js
+++ js/eq.js
@@ -17,74 +17,62 @@ document.getElementById('play')?.addEventListener('click', async () => {
 		player1.stop();
 	})
 
-	 var eq1 = new Tone.EQ3({
-		low:"8",
-		mid:"8",
-		high:"8"
+	var eq1 = new Tone.EQ3({
+		low: "8",
+		mid: "8",
+		high: "8"
 	}).toDestination();
-	
+
 
 	// connect the player to the eq
 	player1.connect(eq1)
 
 
-	
-	sliderLow.on('change',function(v) {
-		
+
+	sliderLow.on('change', function (v) {
 		eq1.low.value = sliderLow.value;
-		
-		
 		console.log(eq1);
-	  });
+	});
 
-	  sliderMid.on('change',function(v) {
-		
-		
+	sliderMid.on('change', function (v) {
 		eq1.mid.value = sliderMid.value;
-		
-		
 		console.log(eq1);
-	  });
+	});
 
-	  sliderHigh.on('change',function(v) {
-	
-		
+	sliderHigh.on('change', function (v) {
 		eq1.high.value = sliderHigh.value;
-		
 		console.log(eq1);
-	  });
-
-	  
-
-	
-	
+	});
 
 })
 var sliderLow = new Nexus.Slider('#low', {
-	'size': [120,20],
-    'mode': 'absolute',  // 'relative' or 'absolute'
-    'min': 0,
-    'max': 10,
-    'step': 0,
+	'size': [120, 20],
+	'mode': 'relative',  // 'relative' or 'absolute'
+	'min': 0,
+	'max': 10,
+	'step': 0,
 	'value': 8,
+	'mode': 'line'
 })
 
 var sliderMid = new Nexus.Slider('#mid', {
-	'size': [120,20],
-    'mode': 'absolute',  // 'relative' or 'absolute'
-    'min': 0,
-    'max': 10,
-    'step': 0,
-    'value': 8
+	'size': [120, 20],
+	'mode': 'relative',  // 'relative' or 'absolute'
+	'min': 0,
+	'max': 10,
+	'step': 0,
+	'value': 8,
+	'mode': 'bar'
 })
 
 var sliderHigh = new Nexus.Slider('#high', {
-	'size': [120,20],
-    'mode': 'absolute',  // 'relative' or 'absolute'
-    'min': 0,
-    'max': 10,
-    'step': 0,
-    'value': 8
+	'size': [120, 20],
+	'mode': 'relative',  // 'relative' or 'absolute'
+	'min': 0,
+	'max': 10,
+	'step': 0,
+	'value': 8,
+	'mode': 'bar'
 })
 
 
